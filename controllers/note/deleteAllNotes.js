d9cd63f3d76b9bac9ad75a9358c9a8243d70702b
Ahmed Hamdy <hamdimn54@gmail.com>
2021-08-28 21:50:01 +0200

    deleteAll fix attempt #2

--- controllers/note/deleteAllNotes.js
+++ controllers/note/deleteAllNotes.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { noteModel } = require('../../models');
 
-const deleteNote = async (id, user) => {
+const deleteNote = async (user) => {
 	return new Promise(async (resolve, reject) => {
 		noteModel.deleteMany({ author: user._id }, (err, res) => {
 			if (err) reject(err);
